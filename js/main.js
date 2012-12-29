a7972ee558b35751a63c5c5bd79070f7b26fd922
Rex Feng <rexfeng@gmail.com>
2012-12-28 19:21:39 -0800

    add roll dice button

--- js/main.js
+++ js/main.js
@@ -1,23 +1,17 @@
 /* InstaDice 2012 by rexfeng */
 $(document).ready(function() {
 
-  var random_dice_roll  = function(){ return Math.floor((Math.random()*6)+1); },
-      dice_one          = random_dice_roll(),
-      dice_two          = random_dice_roll();
+  var random_dice_roll  = function(){ return Math.floor((Math.random()*6)+1); };
 
   // on page load, insert new dice roll
-    $('#dice-one').text(dice_one);
-    $('#dice-two').text(dice_two);
+    $('#dice-one').text(random_dice_roll());
+    $('#dice-two').text(random_dice_roll());
 
-  // // on click #roll_the_dice
-  // $('#roll-the-dice').click(function() {
-
-  // // move dice to Past Rolls
-
-  // // generate two random 1-12 in Latest Roll
-  //   $('#dice-one').html(dice_one);
-  //   $('#dice-two').html(dice_two);
-  // });
+  // on click #roll_the_dice
+  $('#re-roll').click(function() {
+    $('#dice-one').text(random_dice_roll());
+    $('#dice-two').text(random_dice_roll());
+  });
 
 
 
