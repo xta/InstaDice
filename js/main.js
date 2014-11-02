ee6f34314dcaf6f7c3a1536d34b5c5fd2122422a
Rex Feng <rexfeng@gmail.com>
2014-11-02 01:15:44 -0700

    add log for dice roll. update basic styling

--- js/main.js
+++ js/main.js
@@ -1,18 +1,46 @@
-/* InstaDice 2012 by rexfeng */
+/* InstaDice 2012-2014 by rexfeng */
 $(document).ready(function() {
 
   var random_dice_roll  = function(){ return Math.floor((Math.random()*6)+1); };
 
+  var current_time = function(){
+    var now     = new Date(),
+        year    = now.getFullYear(),
+        month   = now.getMonth()+1,
+        day     = now.getDate(),
+        hour    = now.getHours(),
+        minute  = now.getMinutes(),
+        second  = now.getSeconds(),
+        dateTime;
+
+    if(minute.toString().length == 1) {
+      minute = '0'+minute;
+    }
+    if(second.toString().length == 1) {
+      second = '0'+second;
+    }
+    dateTime = year+'/'+month+'/'+day+' '+hour+':'+minute+':'+second;
+    return dateTime;
+  };
+
+  var roll_dice = function(){
+    var roll_one = random_dice_roll(),
+        roll_two = random_dice_roll(),
+        event_log;
+
+    $('#dice-one').text(roll_one);
+    $('#dice-two').text(roll_two);
+
+    event_log = 'Time: ' + current_time() +'. Dice: [' + roll_one + '], [' + roll_two + '].';
+    $("ul#log").prepend($("<li></li>").html(event_log));
+  };
+
   // on page load, insert new dice roll
-    $('#dice-one').text(random_dice_roll());
-    $('#dice-two').text(random_dice_roll());
+  roll_dice();
 
   // on click #roll_the_dice
   $('#re-roll').click(function() {
-    $('#dice-one').text(random_dice_roll());
-    $('#dice-two').text(random_dice_roll());
+    roll_dice();
   });
-
-
 
 });
